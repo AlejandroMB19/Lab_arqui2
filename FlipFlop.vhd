fee508f97b34c35922911cb71588c0097d5238f3
santiflo <santiflo@javerianacali.edu.co>
2018-09-28 12:37:04 -0500

    Update FlipFlop.vhd

--- FlipFlop.vhd
+++ FlipFlop.vhd
@@ -4,13 +4,16 @@ use ieee.std_logic_1164.all;
 entity FlipFlop is
 port(
 	D : in std_logic;
-	Clock : in std_logic;
+	CLK : in std_logic;
 	Q: out std_logic);
 end entity;
 
 architecture FlipFlop_arch of flipFlop is
 begin
-	process(D,Clock)
-		Q <= ;
+	process(CLK)
+		begin
+			if(CLK = '1' and (CLK'EVENT)) then
+				Q <= D;
+			end if;
 	end process;
-end architecture;
+end FlipFlop_arch;
